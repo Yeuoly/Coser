cab5aaaed20f166f327168b157eb6ea260cffc11
Yeuoly <admin@srmxy.cn>
2023-11-13 02:57:16 +0800

    better selection

--- front/src/components/CameraEditor.vue
+++ front/src/components/CameraEditor.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="c-c-camera-editor">
         <NInputGroup>
-            <NAutoComplete v-model:value="camera.camera" placeholder="相机"></NAutoComplete>
-            <NAutoComplete v-model:value="camera.lens" placeholder="镜头"></NAutoComplete>
+            <NAutoComplete :options="cameraOptions" v-model:value="camera.camera" placeholder="相机"></NAutoComplete>
+            <NAutoComplete :options="lensOptions" v-model:value="camera.lens" placeholder="镜头"></NAutoComplete>
         </NInputGroup>
         <NInputGroup>
             <NInputNumber v-model:value="camera.focal" placeholder="焦段">
@@ -35,7 +35,20 @@
 <script setup lang="ts">
 import { NAutoComplete, NInput, NInputGroup, NInputNumber, NSelect } from 'naive-ui'
 
-import { EXIF } from '../utils/camera'
+import { EXIF, cameras, lens } from '../utils/camera'
+import { computed } from 'vue'
+
+const cameraOptions = computed(() => {
+    return cameras.filter(
+        v => v.toLowerCase().includes(camera.value.camera.toLowerCase())
+    ).map(v => ({ label: v, value: v }))
+})
+
+const lensOptions = computed(() => {
+    return lens.filter(
+        v => v.toLowerCase().includes(camera.value.lens.toLowerCase())
+    ).map(v => ({ label: v, value: v }))
+})
 
 const camera = defineModel<EXIF>('camera', {
     required: true
